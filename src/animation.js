a98632151111e01b01725477205ce704f9839b22
Ruben Vreeken <ruben.vreeken@gmail.com>
2013-10-14 13:10:25 +0200

    Extracted WindowAnimationTiming polyfill out of Animation and into its own component.

--- src/animation.js
+++ src/animation.js
@@ -1,41 +1,13 @@
 /**
  * TODO: inertia
  */
-var Animation = (function(Type, Style, Event, Easing, window) {
+var Animation = (function(WindowAnimationTiming, Type, Style, Event, Easing) {
 
   "use strict";
 
   // Local WindowAnimationTiming interface
-  var cancelAnimationFrame  = window.cancelAnimationFrame || window.cancelRequestAnimationFrame;
-  var requestAnimationFrame = window.requestAnimationFrame;
-
-  // Local WindowAnimationTiming interface polyfill
-  (function (window) {
-    var i, vendors = ['moz', 'webkit', 'o', 'ms'], lastTime = 0;
-
-    // For a more accurate WindowAnimationTiming interface implementation, ditch the native
-    // requestAnimationFrame when cancelAnimationFrame is not present (older versions of Firefox)
-    for(i = 0; i < 3 && !cancelAnimationFrame; ++i) {
-      cancelAnimationFrame  =  window[vendors[i]+'CancelAnimationFrame']
-                            || window[vendors[i]+'CancelRequestAnimationFrame'];
-      requestAnimationFrame =  cancelAnimationFrame
-                            && window[vendors[i]+'RequestAnimationFrame'];
-    }
-
-    if (!cancelAnimationFrame) {
-      requestAnimationFrame = function(callback) {
-        currentTime, timeToCall;
-        currentTime = +new Date();
-        timeToCall  = Math.max(0, 16 - (currentTime - lastTime));
-        lastTime    = currentTime + timeToCall;
-        return window.setTimeout(function() { callback(currentTime + timeToCall); }, timeToCall);
-      };
-
-      cancelAnimationFrame = function (id) {
-        clearTimeout(id);
-      };
-    }
-  }(window));
+  var cancelAnimationFrame  = WindowAnimationTiming.cancelAnimationFrame;
+  var requestAnimationFrame = WindowAnimationTiming.requestAnimationFrame;
 
   var Animation = function(element) {
     if(!Type.is('Element', element)) { return; }
@@ -332,4 +304,4 @@ var Animation = (function(Type, Style, Event, Easing, window) {
 
   return Animation;
 
-})(Type, Style, Event, 'undefined' != typeof EasingFunctions ? EasingFunctions : {}, window);
+})(WindowAnimationTiming, Type, Style, Event, 'undefined' != typeof EasingFunctions ? EasingFunctions : {});
