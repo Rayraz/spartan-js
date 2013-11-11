5df31fee5849f00ddaff932d8d81934b049dae4a
Ruben Vreeken <ruben.vreeken@gmail.com>
2013-11-11 17:40:36 +0100

    Added a small library with handy object manipulation functions. So far these include: - Keys, return all keys of an object. - Values, return all values of an object. - Extend, extend one object with another (or multiple others). Optionally recursive. (IMPORTANT! Not meant for classy inheritance etc.)

---
+++ src/obj.js
@@ -0,0 +1,55 @@
+var Obj = (function(Type) {
+
+	"use strict";
+
+	return {
+		extend: function(object, /* polymorphic, */ recursive) {
+			var sources
+				, i
+				, n
+				, source
+				, property;
+
+			sources   = Array.prototype.slice.call(arguments, 1);
+			recursive = sources.pop();
+			if(recursive !== true) {
+				sources.push(recursive);
+				recursive = false;
+			}
+
+			for(i = 0, n = sources.length; i < n; i++) {
+				source = sources[i];
+				for(property in source) {
+					if(object[property] && Type.is('Object', object[property]) && recursive) {
+						object[property] = this.extend.call(this, object[property], source[property], recursive);
+					}
+					else {
+						object[property] = source[property];
+					}
+				}
+			}
+			return object;
+		},
+		keys: function(object) {
+			var result = []
+				, i;
+
+			for(i in object) {
+				result.push(i);
+			}
+
+			return result;
+		},
+		values: function(object) {
+			var result = []
+				, i;
+
+			for(i in object) {
+				result.push(object[i]);
+			}
+
+			return result;
+		}
+	};
+
+})(Type);
