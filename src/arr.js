2bc119f25387c6fdc63c51421b55d6395db9fa03
Ruben Vreeken <ruben.vreeken@gmail.com>
2013-11-11 17:17:56 +0100

    Added indexOf and contains methods. Now storing array-length in local variable during for loops.

--- src/arr.js
+++ src/arr.js
@@ -6,10 +6,11 @@ var Arr = (function() {
 		unique: function(arr) {
 			var result = []
 				, i
+				, n
 				, j
 				, match;
 
-			for(i = 0; i < arr.length; i++) {
+			for(i = 0, n = arr.length; i < n; i++) {
 				match = false;
 				for(j in result) {
 					match = arr[i] === result[j];
@@ -26,24 +27,43 @@ var Arr = (function() {
 		},
 		replace: function(arr, remove, add, unique) {
 			var result = []
-				, i;
+				, i
+				, n;
 
-			for(i = 0; i < arr.length; i++) {
+			for(i = 0, n = arr.length; i < n; i++) {
 				result.push((arr[i] === remove) ? add : arr[i]);
 			}
 			return (unique) ? this.unique(result) : result;
 		},
 		remove: function(arr, value) {
 			var result = []
-				, i;
+				, i
+				, n;
 
-			for(i = 0; i < arr.length; i++) {
+			for(i = 0, n = arr.length; i < n; i++) {
 				if(arr[i] !== value) {
 					result.push(arr[i]);
 				}
 			}
 			return result;
-		}
+		},
+		indexOf: function(arr, value) {
+      var i
+      	, n;
+
+      // find index of value
+      for(i = 0, n = arr.length; i < n; i++) {
+        if(this.array[i] == value) {
+          return i;
+        }
+      }
+
+      // not found
+      return -1;
+    },
+    contains: function(arr, value) {
+    	return this.indexOf(arr, value) > -1;
+    }
 	};
 
 })();
